ff7562ddb9e066b78653d1e94eb5c328cae20847
KritsadaP <49741993+KritsadaP@users.noreply.github.com>
2019-04-19 13:15:38 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,9 +1,9 @@
 <?php
 
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'VEPSiyd0i9Cl1wuJp0KsXbQqfv0mDhQt6qGSXdxUlx4qr5qCsUAHAYbWRzeMWh9KN+/S0xzAQiyFXOku7X+kISurW4j3hcT932DmZE1EazoZF7FJrNuRnqQ3+QcDEVg0BhOxCxwESGZI2SyGdyHXVgdB04t89/1O/w1cDnyilFU=';
 
-$userId = 'Uffa138efe037e6e889d0b0f4a871c005';
+$userId = 'Ub3d8546f4974779e5a938055a2642f24';
 
 $url = 'https://api.line.me/v2/bot/profile/'.$userId;
 
