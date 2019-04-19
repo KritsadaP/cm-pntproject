c1a2f6255f12ece8cc422f2b4267dc5b3511d333
KritsadaP <49741993+KritsadaP@users.noreply.github.com>
2019-04-19 12:46:26 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'VEPSiyd0i9Cl1wuJp0KsXbQqfv0mDhQt6qGSXdxUlx4qr5qCsUAHAYbWRzeMWh9KN+/S0xzAQiyFXOku7X+kISurW4j3hcT932DmZE1EazoZF7FJrNuRnqQ3+QcDEVg0BhOxCxwESGZI2SyGdyHXVgdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
